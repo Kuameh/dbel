181d1eceb7a0e659ee304e44904c149ddf43a9ff
Ashiangmor Caleb <56304597+Kuameh@users.noreply.github.com>
2023-12-26 12:29:08 +0000

    Add NotFound component

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <main>
+      <section className="flex h-[75vh] flex-col items-center justify-center">
+        <h1 className="text-6xl font-semibold italic">404</h1>
+        <h2 className="font-extralight italic">Page not found</h2>
+      </section>
+    </main>
+  );
+};
+
+export default NotFound;
