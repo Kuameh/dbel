4c92b9ad9c501a3abb719c786a9749ff3eaac50c
Ashiangmor Caleb <56304597+Kuameh@users.noreply.github.com>
2023-12-26 12:29:45 +0000

    Add Footer component

---
+++ src/shared/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="mt-8 flex items-center justify-center py-4 italic text-gray-500">
+      <p>Made with ❤ by Brocli Studio</p>
+    </footer>
+  );
+};
+
+export default Footer;
