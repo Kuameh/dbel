74d5d9144205479d1db85f5dc8c2fc275368d511
Ashiangmor Caleb <56304597+Kuameh@users.noreply.github.com>
2023-12-26 14:15:54 +0000

    Refactor App component to use React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./shared/Layout";
+import Program from "./pages/Program";
+import NotFound from "./pages/NotFound"; // Import the NotFound component
 
 function App() {
   return (
-    <Fragment>
-      <div>
-      </div>
-    </Fragment>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Program />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
